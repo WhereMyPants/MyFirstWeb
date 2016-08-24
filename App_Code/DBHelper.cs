e0468179c16306d508332a21eaab816709fb7f46
WhereMyPants <495904140@qq.com>
2016-08-24 17:43:43 +0800

    添加引用的类库
    数据逻辑层的类在赋予一个命名空间后，需要添加到App_Code中才能在其他的.cs文件中通过using+命名空间来进行调用！
    这里申明了一个类专门处理SQL语句。

---
+++ App_Code/DBHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+///DBHelper 的摘要说明
+/// </summary>
+
+namespace DAL
+{
+    public class DBHelper
+    {
+        public DBHelper()
+        {
+            //
+            //TODO: 在此处添加构造函数逻辑
+            //
+        }
+    }
+}
--- UI层/Login.aspx.cs
+++ UI层/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DAL;
 
 public partial class UI层_Login : System.Web.UI.Page
 {
